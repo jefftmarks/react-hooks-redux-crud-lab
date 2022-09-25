31a1545fe7c73216a40edf8c9c32e21f7ad199b1
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-25 19:46:28 -0400

    commit tree 87f0a8679ea578a16c574be9e93e6c5fa97c1b04

--- src/features/reviews/Reviews.js
+++ src/features/reviews/Reviews.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Review from "./Review";
 
-function Reviews() {
-  return <ul>Reviews</ul>;
+function Reviews({ reviews }) {
+
+	const reviewList = reviews.map((review) => (
+		<Review key={review.id} review={review}/>
+	))
+
+  return <ul>{reviewList}</ul>;
 }
 
 export default Reviews;
